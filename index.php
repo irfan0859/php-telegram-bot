803dddcd158a8b78849e9e8ac207164e58b584fd
irfan0859 <30881484+irfan0859@users.noreply.github.com>
2018-09-09 10:06:42 +0300

    Update index.php

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ require 'vendor/autoload.php';
 
 use Smoqadam\Telegram;
 
-$tg = new Telegram('API_TOKEN');
+$tg = new Telegram('595596482:AAEen3_otWTMOLh0jOEueaJNoYrn5QKX0EU');
 
 $tg->cmd('name:<<[a-zA-Z]{0,}>>', function ($args) use ($tg){
 	
